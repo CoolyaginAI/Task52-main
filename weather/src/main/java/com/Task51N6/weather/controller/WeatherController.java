c9f8557373ca2b5b465be6f85162bedd0c514873
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-07-04 17:03:20 +0300

    First

--- weather/src/main/java/com/Task51N6/weather/controller/WeatherController.java
+++ weather/src/main/java/com/Task51N6/weather/controller/WeatherController.java
@@ -1,17 +1,13 @@
 package com.Task51N6.weather.controller;
 
 import com.Task51N6.weather.model.Root;
-import com.Task51N6.weather.model.Weather;
-import com.Task51N6.weather.repository.WeatherRepository;
+import com.Task51N6.weather.model.Sys;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import com.Task51N6.weather.model.Main;
 
-import java.util.Iterator;
-import java.util.Optional;
 
 @RestController
 public class WeatherController {
@@ -20,11 +16,15 @@ public class WeatherController {
     private RestTemplate restTemplate;
 
     @Value("${appid}")
-    String appId;
+    private String appId;
+
+    @Value("${url.weather}")
+    private String urlWeather;
 
     @GetMapping("/weather")
-    public Root getWeather() {
-        return  restTemplate.getForObject("https://api.openweathermap.org/data/3.0/onecall?lat=54.1838&lon=45.1749&units=metric&appid=" + appId, Root.class);
+    public Main getWeather(@RequestParam String lat, @RequestParam String lon) {
+        String request = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s", urlWeather, lat, lon, appId);
+        return  restTemplate.getForObject(request, Root.class).getMain();
     }
 
 }
--- weather/src/main/java/com/Task51N6/weather/repository/WeatherRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Task51N6.weather.repository;
-
-import com.Task51N6.weather.model.Weather;
-import org.springframework.data.repository.CrudRepository;
-
-public interface WeatherRepository extends CrudRepository<Weather, Integer> {
-}
