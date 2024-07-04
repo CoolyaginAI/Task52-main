df94cb882f613ae95ac188063331aed31ae62012
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-07-04 14:57:41 +0300

    First

--- weather/src/main/java/com/Task51N6/weather/controller/WeatherController.java
+++ weather/src/main/java/com/Task51N6/weather/controller/WeatherController.java
@@ -1,8 +1,10 @@
 package com.Task51N6.weather.controller;
 
+import com.Task51N6.weather.model.Root;
 import com.Task51N6.weather.model.Weather;
 import com.Task51N6.weather.repository.WeatherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,42 +14,17 @@ import java.util.Iterator;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/weather")
 public class WeatherController {
 
     @Autowired
-    private WeatherRepository weatherRepository;
+    private RestTemplate restTemplate;
 
-    @GetMapping
-    public Iterable<Weather> findAll() {
-        return weatherRepository.findAll();
-    }
+    @Value("${appid}")
+    String appId;
 
-    @GetMapping("/{id}")
-    public Optional<Weather> findById(@PathVariable int id) {
-        return weatherRepository.findById(id);
-    }
-
-    @PostMapping
-    public ResponseEntity<Weather> save(@RequestBody Weather weather) {
-        return  weatherRepository.findById(weather.getId()).isPresent()
-                ? new ResponseEntity(weatherRepository.findById(weather.getId()), HttpStatus.BAD_REQUEST)
-                : new ResponseEntity(weatherRepository.save(weather), HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{lat}/{lon}")
-    public Optional<Weather> findByLocation(@PathVariable int lat, @PathVariable int lon) {
-        Iterable<Weather> tempWeather = weatherRepository.findAll();
-        Optional<Weather> findWeather = Optional.empty();
-
-        for (Weather i : tempWeather) {
-            if ((i.getLat() == lat)&&(i.getLon() == lon)) {
-                findWeather = Optional.ofNullable(i);
-                break;
-            }
-        }
-
-        return findWeather;
+    @GetMapping("/weather")
+    public Root getWeather() {
+        return  restTemplate.getForObject("https://api.openweathermap.org/data/3.0/onecall?lat=54.1838&lon=45.1749&units=metric&appid=" + appId, Root.class);
     }
 
 }
